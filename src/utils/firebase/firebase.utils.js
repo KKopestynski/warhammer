cf8df257b951b8b736e2d996cf081f179bf3379a
kkopestynski <k.kopestynski@gmail.com>
2023-02-19 21:43:01 +0100

    Section 6 Authentication plus Firebase - intitial config, sign in component, firebase create user

---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,55 @@
+import {initializeApp} from 'firebase/app';
+import {getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider, userAuth} from 'firebase/auth';
+import {getFirestore, doc, getDoc, setDoc} from 'firebase/firestore'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyB95nfqhMCKG0BxUHWrDPeG-_cNsU6y6AM",
+    authDomain: "warhammer-shop.firebaseapp.com",
+    projectId: "warhammer-shop",
+    storageBucket: "warhammer-shop.appspot.com",
+    messagingSenderId: "909404899865",
+    appId: "1:909404899865:web:b53f9d23cd4d59a99f3888",
+    measurementId: "G-N53DSYB4LD"
+  };
+  
+  // Initialize Firebase
+  const firebaseApp = initializeApp(firebaseConfig);
+//   const analytics = getAnalytics(app);
+
+
+const provider = new GoogleAuthProvider();
+provider.setCustomParameters({
+    prompt: "select_account"
+})
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+//allows / creates a DB
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+    const userDocRef = doc(db, 'users', userAuth.uid);
+    console.log(userDocRef);
+
+    const userSnapshop = await getDoc(userDocRef);
+    console.log(userSnapshop);
+    console.log(userSnapshop.exists());
+
+    if(!userSnapshop.exists()){
+        const {displayName, email} = userAuth;
+        const createAt = new Date();
+
+        try {
+            await setDoc(userDocRef, {displayName, email, createAt});
+        }catch (error) {
+            console.log('error creating the user', error.message)
+        }
+    }
+    return userDocRef;
+
+    //if check if user data exists
+    //if check if user data don't exists
+
+    //return userDocRef
+}
